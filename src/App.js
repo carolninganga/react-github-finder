d340125e090d03696f8d1bad3014b91b16d19110
Caroline Ninganga <carolninganga@gmail.com>
2020-06-23 12:22:04 -0400

    added a Navbar Component

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './components/layout/Navbar'
+import './App.css';
+
+class App extends React.Component {
+
+  render() {
+  return (
+
+    <div>
+    <Navbar />
+    </div>
+  
+   
+  );
+}
+}
+
+export default App;
