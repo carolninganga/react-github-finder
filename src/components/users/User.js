071d02019aeec104d356f8d16c3c73468024c0fa
Caroline Ninganga <carolninganga@gmail.com>
2020-06-24 09:52:35 -0400

    added user profile page

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,31 +1,100 @@
-import React from 'react';
-import UserItem from './UserItem';
+import React, { Component, Fragment } from 'react';
 import Spinner from '../layout/spinner';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Users = ({ users, loading }) => {
-    if(loading) {
-        return <Spinner />
-    } else {
+class User extends Component {
+    componentDidMount() {
+        this.props.getUser(this.props.match.params.login);
+    }
+
+    static propTypes = {
+        loading: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired,
+    }
+
+    render() {
+        const { 
+            name,
+            company,
+            avatar_url,
+            location,
+            bio,
+            blog,
+            login,
+            html_url,
+            followers,
+            following,
+            public_repos,
+            public_gists,
+            hireable
+                } = this.props.user;
+
+        const  { loading } = this.props;
+        if(loading) return <Spinner />
+
+
         return (
-            <div style={userStyle}>
-                {users.map(user => (
-                    <UserItem key={user.id} user={user} />
-                ))}
-            </div>
-        )
+            <Fragment>
+                <Link to='/' className='btn btn-light'>
+                    Back To Search
+                </Link>
+                Hireable:{' '}
+                {hireable ? (
+                    <i className='fas fa-check text-success' />
+                ) : (
+                    <i className='fas fa-times-circle text-danger' />
+                )}
+                <div className='card grid-2'>
+                    <div className='all-center'>
+                        <img
+                            src={avatar_url}
+                            className='round-img'
+                            alt=''
+                            style={{ width: '150px' }}
+                            />
+                            <h1>{name}</h1>
+                            <p>Location: {location}</p>
+                    </div>
+                    <div>
+                        {bio && (
+                        <Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                        </Fragment>
+                        )}
+                        <a href={html_url} className='btn btn-dark my-1'>
+                            Visit Github Profile
+                        </a>
+                        <ul>
+                            <li>
+                                {login && <Fragment>
+                                    <strong>UserName: </strong> {login}
+                                </Fragment>}
+                            </li>
+                            <li>
+                                {company && <Fragment>
+                                    <strong>Company: </strong> {company}
+                                </Fragment>}
+                            </li>
+                            <li>
+                                {blog && <Fragment>
+                                    <strong>Website: </strong> {blog}
+                                </Fragment>}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="card text-center">
+                    <div className='badge badge-info'>Followers: {followers}</div>
+                    <div className='badge badge-success'>Following: {following}</div>
+                    <div className='badge badge-danger'>Public Repos: {public_repos}</div>
+                    <div className='badge badge-dark'>Public Gists {public_gists}</div>
+                </div>
+            </Fragment>
+        );
     }
 }
 
-Users.propTypes = {
-    users: PropTypes.array.isRequired,
-    loading: PropTypes.bool.isRequired
-}
-
-const userStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)',
-    gridGap: '1rem'
-}
-
-export default Users;
+export default User
