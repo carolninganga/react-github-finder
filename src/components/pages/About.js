2ed30418c05f7ae9311bcd490d7b516adf585e5a
Caroline Ninganga <carolninganga@gmail.com>
2020-07-06 12:40:23 -0400

    revised the the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <Fragment>
             <h1>About This App</h1>
-            <p>This appication users the Github API to help search users and their repos on github.</p>
+            <p>This appication will allow the Github API to help search users and their repos on github.</p>
             <p>Version: 1.0.0</p>
         </Fragment>
     )
